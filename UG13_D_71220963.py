b1fe9aa44a1ea21243be71b9003ee5fc4b09848e
Paetttt <118429357+Paetttt@users.noreply.github.com>
2022-12-15 16:01:27 +0700

    Add files via upload

---
+++ UG13_D_71220963.py
@@ -0,0 +1,16 @@
+import turtle
+lol=turtle.Turtle()
+lol.pensize(20)
+
+lol.color("white")
+lol.screen.bgcolor("purple")
+lol.circle(80,180)
+lol.left(90)
+lol.forward(250)
+ 
+lol. up
+lol.pensize(20)
+lol.circle(50,180)
+lol.right(180)
+lol.circle(50,180)
+lol=turtle.Screen().exitonclick()
