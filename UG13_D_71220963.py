78a38af2ab8c3f4cf03e5a1deb8ab715d821c8bb
Paetttt <118429357+Paetttt@users.noreply.github.com>
2022-12-15 16:20:16 +0700

    Add files via upload

--- UG13_D_71220963.py
+++ UG13_D_71220963.py
@@ -4,12 +4,19 @@ lol.pensize(20)
 
 lol.color("white")
 lol.screen.bgcolor("purple")
-lol.circle(80,180)
-lol.left(90)
-lol.forward(250)
- 
-lol. up
-lol.pensize(20)
+lol.penup()
+lol.setpos(50,180)
+lol.pendown()
+
+lol.forward(30)
+lol.backward(30)
+
+lol.circle(-40,-185)
+lol.circle(40,-250)
+
+lol.up()
+lol.goto(200,-40)
+lol.pendown()
 lol.circle(50,180)
 lol.right(180)
 lol.circle(50,180)
